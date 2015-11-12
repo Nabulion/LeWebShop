87aadc459b327cd1aa68d471194c2aa2ff00f3df
user.email <bnm>
2015-11-12 09:08:05 +0100

    Dao Service

---
+++ WebshopAdmin/Webshop/Dao/Dao.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webshop.Dao
+{
+    public class Dao
+    {
+        private static lewebshopEntities DB;
+
+        public static lewebshopEntities getDB()
+        {
+            if (DB == null)
+            {
+                DB = new lewebshopEntities();
+            }
+            return DB;
+        }
+        public static LoginUser findUser(String pass)
+        {
+            var LoginUser = from l in DB.LoginUsers where l.pass == pass select l;
+            return (LoginUser)LoginUser;
+        }
+        public static void updateUser(LoginUser u, String newname, String newpass)
+        {
+            u.name = newname;
+            u.pass = newpass;
+            DB.SaveChanges();
+        }
+    }
+}
