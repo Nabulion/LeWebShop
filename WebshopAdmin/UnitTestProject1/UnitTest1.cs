8fb27f9302422c9e7cc49c2ad0c1061fa45a7f7a
jensBrauner <jenstbrauner@gmail.com>
2015-11-19 12:06:55 +0100

    grege

--- WebshopAdmin/UnitTestProject1/UnitTest1.cs
+++ WebshopAdmin/UnitTestProject1/UnitTest1.cs
@@ -24,32 +24,60 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestCreateProduct()
         {
-            service.createProduct("Test ost", (decimal)20, 20, @"C:\Users\Jens\Source\Repos\LeWebShop2\WebshopAdmin\Webshop\pic\ost3.jpg", "Danmark", "Test kategori", "osten smager af mug",  true);
-            
+            service.createProduct("Test ost", (decimal)20, 20, @"C:\Users\Jens\Source\Repos\LeWebShop2\WebshopAdmin\Webshop\pic\ost3.jpg", "Danmark", "Test kategori", "MMMMMM OSTE",true);
+
             var queryProduct = from product in db.Products
                                where product.name == "Test Ost"
                                select product;
 
-            BitmapImage img = service.getImage(@"C:\Users\Jens\Source\Repos\LeWebShop2\WebshopAdmin\Webshop\pic\ost3.jpg");
+            //BitmapImage img = service.getImage(@"C:\Users\Jens\Source\Repos\LeWebShop2\WebshopAdmin\Webshop\pic\ost3.jpg");
 
-            WebshopAdmin.Product p = queryProduct.ToList().ElementAt(0);
+            WebshopAdmin.Product p = queryProduct.ToList().ElementAt(1);
             Assert.AreEqual("Test ost", p.name);
             Assert.AreEqual((decimal)20, p.unitPrice);
             Assert.AreEqual(20, p.countAvailable);
-            CollectionAssert.AreEqual(service.convertToByteArray(img), p.picture);
+            //CollectionAssert.AreEqual(service.convertToByteArray(img), p.picture);
             Assert.AreEqual((decimal)0, p.rating);
             Assert.AreEqual("Danmark", p.country);
             Assert.AreEqual("Test kategori", p.category);
-            Assert.AreEqual("osten samger af mug", p.PDescription);
+            Assert.AreEqual("MMMMMM OSTE", p.PDescription);
             Assert.AreEqual(true, p.@new);
         }
 
-        //[TestMethod]
+        [TestMethod]
 
-        //public void TestCreatePackage()
-        //{
-        //    service.createPackage("Danske oste")
-        //}
+        public void TestCreatePackage()
+        {
+            List <Product> l = new List<Product>();
+            l = db.Products.ToList();
+            service.createPackage(l, "Danske oste", 200);
+           
+                
+            var queryPackage = from package in db.Packages
+                               where package.name == "Danske oste"
+                               select package;
+    
+            
+            Package pac = queryPackage.ToList().ElementAt(0);
+            Assert.AreEqual("Danske oste", pac.name);
+            Assert.AreEqual(200, pac.price);
 
+        }
+
+        [TestMethod]
+
+        public void TestCreateFAQ()
+        {         
+            service.createFAQ("LUGTER OSTEN?", "JA");
+            
+             var queryFAQ = from faq in db.FAQs
+                               where faq.question == "LUGTER OSTEN?"
+                               select faq;
+
+            FAQ faq1 =  queryFAQ.ToList().ElementAt(0);
+
+            Assert.AreEqual("LUGTER OSTEN?", faq1.question);
+            Assert.AreEqual("JA", faq1.answer);        
+        }
     }
 }
